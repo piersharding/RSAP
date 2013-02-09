4500640cd3d1ab36ce13808cea5a1ffe57c71ab1
Piers Harding <piers@catalyst.net.nz>
2013-02-10 07:28:35 +1300

    switch to configure.ac instead of multiple Makevars

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm *.gz *.trc *.Rout src/*.i src/*.ii src/Makevars.in; rm -rf *.Rcheck; clear; R CMD build .
+rm *.gz *.trc *.Rout src/*.i src/*.ii; rm -rf *.Rcheck; clear; R CMD build .
 FLE=`ls RSAP_*.tar.gz`
 echo "Eyeball the contents for wrong files:"
 tar -tzvf $FLE
